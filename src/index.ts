29471c774250bdad2f11dadf8a349019fbb3b227
mc_fdc <Masato.04.11.2007@gmail.com>
2023-06-30 20:05:16 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,7 @@ const app = express();
     };
     let page = await browser.newPage();
     await page.goto(req.query.url as string);
-    let content = await page.screenshot_1({
-      encoding: "binary"
-    }) as Buffer;
+    let content = await page.screenshot() as Buffer;
     await page.close();
     await res.send(content.buffer);
   });
