db8a07f015e4bfa9334a4e8bbc3562d8415c9cba
mc_fdc <Masato.04.11.2007@gmail.com>
2023-06-30 19:54:08 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const app = express();
     }
     let page = await browser.newPage();
     await page.goto(req.query.url as string);
+    await page.close();
     await res.send("Ok");
   });
 })();
