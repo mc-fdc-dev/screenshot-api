26c9af124f9ee65a57328d5694144c4ebf5fa72e
mc_fdc <Masato.04.11.2007@gmail.com>
2023-07-01 11:00:11 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,36 @@ import { promises as dns, ADDRCONFIG } from "dns";
 
 const app = express();
 
-(async () => {
+app.get("/", async (req, res) => {
   const { address } = await dns.lookup(process.env.BROWSER_ADDRESS as string, {
     family: 4,
     hints: ADDRCONFIG,
-  });
+   });
   if (!address) {
     return;
   }
   const browser = await puppeteer.connect({ browserURL: `http://${address}:${process.env.BROWSER_PORT}` });
-
-  app.get("/", async (req, res) => {
-    if (!req.query.url) {
-      return await res.send({
-        "message": "invalid"
-      });
-    };
-    let page = await browser.newPage();
-    await page.setViewport({
-      height: 960,
-      width: 1260,
+  if (!req.query.url) {
+    return await res.send({
+      "message": "invalid"
     });
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto(req.query.url as string, {
-       waitUntil: "networkidle2",
-    });
-    let content = await page.screenshot() as Buffer;
-    await page.close();
-    // return image data
-    res.writeHead(200, {
-      "Content-Type": "image/png",
-      "Content-Length": content.length
-    });
-    res.end(content);
+  };
+  let page = await browser.newPage();
+  await page.setViewport({
+    height: 960,
+    width: 1260,
   });
-})();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(req.query.url as string, {
+    waitUntil: "networkidle2",
+  });
+  let content = await page.screenshot() as Buffer;
+  await page.close();
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": content.length
+  });
+  res.end(content);
+});
 
 app.listen(3000);
